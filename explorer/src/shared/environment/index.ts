c61881ed44abe3f3f84bb0385cc1706982b16d06
Dawid Motyka <motykadaw@gmail.com>
2022-05-04 17:12:53 +0200

    use temp env

--- explorer/src/shared/environment/index.ts
+++ explorer/src/shared/environment/index.ts
@@ -1,4 +1,5 @@
-export const baseGqlUrl = process.env.REACT_APP_API_URL;
+// export const baseGqlUrl = process.env.REACT_APP_API_URL;
+export const baseGqlUrl = process.env.REACT_APP_TEMP_URL;
 export const gqlEndpoint = `${baseGqlUrl}/api/gateway`;
 export const availableCategoriesEndpoint =
   process.env.REACT_APP_CATEGORIES_API_URL || "";
