f76ed841dd5214e38fc981118959d5351cff9c98
Dawid Motyka <motykadaw@gmail.com>
2022-05-06 09:18:53 +0200

    fix sanity data

--- server/domain/sanity/data.ts
+++ server/domain/sanity/data.ts
@@ -10,14 +10,14 @@ export const mockSanityData: DbSanity[] = [
   {
     userId: "7f3d4ead-0c4a-4ed5-9656-30f914019833",
     // replace `newServiceData` here and in each object below with proper fields defined in schema
-    sanityData: "Example data",
+    sanity: { sanityData: "Example data" },
   },
   {
     userId: "f49e6295-b93e-4f21-b2a9-523ff8d4a068",
-    sanityData: "Example data",
+    sanity: { sanityData: "Example data" },
   },
   {
     userId: "ffe449ec-1571-4ce3-bda3-9bcc65e12c8a",
-    sanityData: "Example data",
+    sanity: { sanityData: "Example data" },
   },
 ];
