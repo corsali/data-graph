bd70e0df25af218675ba52fdd3cb564aeab93d32
Dawid Motyka <motykadaw@gmail.com>
2022-05-06 07:47:46 +0200

    ignore fe build test

--- server/templates/api/index.ts
+++ server/templates/api/index.ts
@@ -5,9 +5,9 @@ import { createSubgraphServer, Service } from "../../shared";
 import { resolvers, typeDefs } from "../../domain/<SERVICE_NAME>";
 
 createSubgraphServer({
-  // 1. go to `server/shared/env/index.ts`.
-  // 2. add `<SERVICE_NAME>: "service_name",` to `Service` enum.
-  // 3. add `[Service.<SERVICE_NAME>]: <next unused number, eg. 4006>` to `localPortsConfig` object.
+  // 1. go to `server/shared/env/index.ts`
+  // 2. add `<SERVICE_NAME>: "service_name",` to `Service` enum
+  // 3. add `[Service.<SERVICE_NAME>]: <next unused number, eg. 4006>` to `localPortsConfig` object
   // 4. replace `MyService` in the following line with correct service name added in step 2.
   name: Service.MyService,
   subgraphSchemaConfig: [{ typeDefs, resolvers }],
